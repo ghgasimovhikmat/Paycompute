673532ffc6d5291e0a5639744df6e65f071760e4
ghgasimovhikmat <gh.gasimovhikmat@gmail.com>
2023-05-04 14:50:49 +0300

    Developing layers and Initial Migration

---
+++ Paycompute.Entity/StudentLoan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Entity
+{
+    public enum StudentLoan
+    {
+        Yes,
+        No
+    }
+}
---
+++ Paycompute.Services/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Paycompute.Services
+{
+    public class Class1
+    {
+
+    }
+}
