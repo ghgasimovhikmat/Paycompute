d255ae97f0e8e1aff65793190775748fa5df1b48
ghgasimovhikmat <gh.gasimovhikmat@gmail.com>
2023-05-06 15:10:12 +0300

    Index-EmployeeController and Interface Implementation

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paycompute.Models;
+using Paycompute.Services;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+           _employeeService = employeeService;
+        }
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetALL().Select(employee => new EmployeeIndexViewModel
+            {
+                Id=employee.Id,
+                EmployeeNo= employee.EmployeeNo,
+                FullName=employee.FullName,
+                Gender=employee.Gender,
+                ImageUrl=employee.ImageUrl,
+                DateJoined=employee.DateJoined,
+                Desigination=employee.Designation,
+                City=employee.City
+
+            }).ToList();
+            return View(employees);
+        }
+    }
+}
